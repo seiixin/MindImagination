38c94425c08f80f5cd7cda3afd0ebfddd38a49df
Seiixin <sidneygarcinan@gmail.com>
2025-06-25 19:39:22 +0800

    update

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@
 import '../css/app.css';
 import './bootstrap';
 
+import { usePage } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
